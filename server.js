05d58e537e1299902753a90fd89595a358abfcf8
jamclean23 <jamclean23@gmail.com>
2023-09-20 12:46:11 -0400

    Add routes for images and fonts

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const contact = readFileSync('./pages/contact/contact.html', 'utf-8');
 const contactJs = readFileSync('./pages/contact/contact.js', 'utf-8');
 const contactCss = readFileSync('./pages/contact/contact.css', 'utf-8');
 
+// Portfolio
+const portfolio = readFileSync('./pages/portfolio/index.html');
+const portfolioJs = readFileSync('./pages/portfolio/main.bundle-ebebc939407d18add80d.js');
+
 // 404
 const fourOhFour = readFileSync('./pages/404/404.html', 'utf-8');
 const fourOhFourJs = readFileSync('./pages/404/404.js', 'utf-8');
@@ -90,12 +94,25 @@ http.createServer((req, res) => {
             break;
         
 
-        // 404
+        // PORTFOLIO
+
+        // Portfolio page
+        case '/portfolio':
+            res.write(portfolio);
+            break;
+        
+        // Portfolio javascript
+        case '/main.bundle-ebebc939407d18add80d.js':
+            res.write(portfolioJs);
+            break;
+
+        // 404 and Image handling
 
         // 404 javascript
         case '/404/404.js':
             res.write(fourOhFourJs);
             break;
+
 
         // 404 css
         case '/404/404.css':
@@ -104,7 +121,19 @@ http.createServer((req, res) => {
             
         // 404 page
         default:
-            res.write(fourOhFour);
+
+            if ((/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i).test(req.url)) {
+                res.setHeader('Content-Type', 'image/png');
+                res.write(readFileSync('./pages/portfolio' + req.url), 'binary');
+            } else if ((/\.(css)$/i).test(req.url)) {
+                res.write(readFileSync('./pages/portfolio' + req.url, 'utf-8'));
+            } else if ((/\.(ttf)$/i).test(req.url)) {
+                console.log('serving font ' + './pages/portfolio' + req.url);
+                res.writeHead(200, { "content-type": "font/ttf" });
+                res.write(readFileSync('./pages/portfolio' + req.url, 'binary'));
+            } else {
+                res.write(fourOhFour);
+            }
             break;
     }
     
